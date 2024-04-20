0ae77029d3f17de312e48a9bc989a08069762a2e
Sten Tijhuis <102481635+Stensel8@users.noreply.github.com>
2024-04-21 00:54:17 +0200

    Update backup-passwordfiles.sh

--- Scripts/scriptmaster-files/Week 2/backup-passwordfiles.sh
+++ Scripts/scriptmaster-files/Week 2/backup-passwordfiles.sh
@@ -10,23 +10,24 @@
 ########################################################################################
 
 function rode_echo() {
-  echo -e "\e[31m$1\e[0m"
+  echo -e "\e[31m$1\e[0m" | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
 }
 
 function oranje_echo() {
-  echo -e "\e[33m$1\e[0m"
+  echo -e "\e[33m$1\e[0m" | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
 }
 
 function groene_echo() {
-  echo -e "\e[32m$1\e[0m"
+  echo -e "\e[32m$1\e[0m" | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
 }
 
 function blauwe_echo() {
-  echo -e "\e[34m$1\e[0m"
+  echo -e "\e[34m$1\e[0m" | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
 }
+
 # Functie om een lijn scheider weer te geven
 function lijn_scheider() {
-  echo "--------------------------------------------------------------"
+  echo "--------------------------------------------------------------" | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
 }
 
 # Controleer of het bestand ServersToBackup is opgegeven als parameter
@@ -53,7 +54,7 @@ mkdir -p "$backup_dir"
 succesvol_aantal=0
 mislukt_aantal=0
 
-echo "Starten van het backupproces voor wachtwoordbestanden..."
+echo "Starten van het backupproces voor wachtwoordbestanden..." | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
 
 # Loop door de servers die zijn afgelezen uit het ServersToBackup-bestand
 for server in "${servers[@]}"; do
@@ -64,12 +65,12 @@ for server in "${servers[@]}"; do
     fi
 
     # Probeer de bestanden /etc/passwd en /etc/shadow van de server te kopiëren
-    ssh "$server" "sudo cp /etc/passwd /etc/shadow ~/" &> /dev/null
+    ssh "$server" "sudo cp /etc/passwd /etc/shadow ~/" &>> backup.log
 
     # Controleer of de kopieeroperatie succesvol was
     if [ $? -eq 0 ]; then
         # Verplaats de gekopieerde bestanden naar de backupdirectory van de server
-        ssh "$server" "sudo mv -i ~/passwd ~/shadow '$server_backup_dir/'" &> /dev/null
+        ssh "$server" "sudo mv -i ~/passwd ~/shadow '$server_backup_dir/'" &>> backup.log
         if [ $? -eq 0 ]; then
             groene_echo "Backup wachtwoordbestanden van $server is gelukt."
             ((succesvol_aantal++))
@@ -85,9 +86,9 @@ for server in "${servers[@]}"; do
 done
 
 lijn_scheider
-blauwe_echo "Backup: $succesvol_aantal servers zijn succesvol geback-upt."
-oranje_echo "Backup: $mislukt_aantal backups zijn mislukt."
-lijn_scheider
+blauwe_echo "Backup: $succesvol_aantal servers zijn succesvol geback-upt." | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
+oranje_echo "Backup: $mislukt_aantal backups zijn mislukt." | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
+lijn_scheider | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
 
 # Geef het pad weer waar de backups zijn gemaakt
-blauwe_echo "Backupbestanden zijn te vinden in: $backup_dir"
+blauwe_echo "Backupbestanden zijn te vinden in: $backup_dir" | tee -a backup.log  # Voeg de uitvoer toe aan het logbestand
