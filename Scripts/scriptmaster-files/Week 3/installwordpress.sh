3d89079031832ffef3fda7edc49d8874a7c845d6
Sten Tijhuis <102481635+Stensel8@users.noreply.github.com>
2024-04-21 14:31:08 +0200

    Testing

--- Scripts/scriptmaster-files/Week 3/installwordpress.sh
+++ Scripts/scriptmaster-files/Week 3/installwordpress.sh
@@ -32,7 +32,7 @@ function lijn_scheider() {
 
 # Functie om verbinding te maken met een server en commando's uit te voeren
 function uitvoeren_op_server() {
-  local server=$1
+  server=$1
   shift
 
   oranje_echo "** Verbinden met $server **"
@@ -41,20 +41,11 @@ function uitvoeren_op_server() {
   # Controleer de exit-status van de SSH-verbinding
   ssh_exit_status=$?
   if [ $ssh_exit_status -ne 0 ]; then
-    rode_echo "Kan geen verbinding maken met $server."
+    rode_echo "Connectie met $server mislukt."
     return 1
   fi
   oranje_echo "** Acties op $server voltooid **"
   echo
-}
-
-# Functie om het IP-adres of de hostname van de MySQL-database te verkrijgen
-function get_mysql_ip() {
-    read -p "Voer het IP-adres of de hostname in voor de MySQL-server: " mysql_ip
-    if [[ -z "$mysql_ip" ]]; then
-        rode_echo "Ongeldige invoer. IP-adres of hostname is vereist. Script afgebroken."
-        exit 1
-    fi
 }
 
 # Controleer of het configuratiebestand als parameter is opgegeven
@@ -75,15 +66,22 @@ fi
 servers=($(grep -v "^#" "$configuratiebestand"))
 
 echo "Installatie via configuratiebestand $configuratiebestand."
-echo "WordPress zal worden geïnstalleerd op de volgende servers: ${servers[*]}"
+echo "Apache zal worden geïnstalleerd en gestart op de volgende servers: ${servers[*]}"
 read -p "Wil je doorgaan (j/n): " keuze
 
 if [ "$keuze" == "j" ]; then
     succes_aantal=0
+    al_geactiveerd_aantal=0
+    al_geactiveerd_servers=""
+    al_geïnstalleerd_niet_actief_servers=""
     installatie_mislukt_aantal=0
 
-    # Vraag het IP-adres of de hostname voor de MySQL-database
-    get_mysql_ip
+    # Vraag het IP-adres voor de MySQL-server
+    read -p "Voer het IP-adres of de hostname van de MySQL-server in: " mysql_server
+    if [ -z "$mysql_server" ]; then
+        rode_echo "Ongeldig IP-adres of hostname. Script afgebroken."
+        exit 1
+    fi
 
     # Loop door servers en voer stappen uit
     for server in "${servers[@]}"; do
@@ -94,28 +92,31 @@ if [ "$keuze" == "j" ]; then
             continue
         fi
         
-        # Installeer benodigde pakketten
-        uitvoeren_op_server $server <<EOF
-            echo "** Benodigde pakketten installeren **"
-            sudo apt update && sudo apt install -y apache2 ghostscript libapache2-mod-php mysql-server php php-bcmath php-curl php-imagick php-intl php-json php-mbstring php-mysql php-xml php-zip
-            if [ \$? -eq 0 ]; then
-                echo "Benodigde pakketten installatie op $server voltooid."
-                ((succes_aantal++))
+        # Controleer of Apache al is geïnstalleerd en actief is
+        if ssh -q $server "sudo systemctl is-active --quiet apache2"; then
+            groene_echo "Apache draait al op $server."
+            ((al_geactiveerd_aantal++))
+            al_geactiveerd_servers+=" $server"
+        elif ssh -q $server "dpkg -l | grep -q apache2"; then
+            oranje_echo "Apache is geïnstalleerd maar niet actief op $server. Probeer te starten..."
+            if ssh -q $server "sudo systemctl start apache2"; then
+                groene_echo "Apache succesvol gestart op $server."
+                ((succes_aantal++))  # Incrementeer het succesvolle aantal voor servers waar Apache succesvol is gestart
             else
-                echo "Benodigde pakketten installatie op $server mislukt."
-                ((installatie_mislukt_aantal++))
+                rode_echo "Kon Apache niet starten op $server. Het lijkt erop dat het pakket beschadigd is."
+                rode_echo "Je zou dit zelf moeten oplossen. Gebruik 'sudo apt autoremove' om dit te doen."
             fi
-EOF
-        lijn_scheider
-        
-        # Installeer WordPress
-        uitvoeren_op_server $server <<EOF
-            echo "** WordPress installeren **"
-            sudo mkdir -p /srv/www
-            sudo chown www-data: /srv/www
-            curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
-            
-            sudo bash -c 'cat << EOFApache > /etc/apache2/sites-available/wordpress.conf
+            al_geïnstalleerd_niet_actief_servers+=" $server"  # Voeg de server toe aan de lijst van al geïnstalleerde maar niet actieve servers
+        else
+            # Installeer en start Apache
+            uitvoeren_op_server $server <<EOF
+                echo "** Apache webserver installeren **"
+                sudo apt update && sudo apt install -y apache2 ghostscript libapache2-mod-php php php-bcmath php-curl php-imagick php-intl php-json php-mbstring php-mysql php-xml php-zip mysql-server
+                if [ \$? -eq 0 ]; then
+                    sudo mkdir -p /srv/www
+                    sudo chown www-data: /srv/www
+                    curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+                    sudo bash -c 'cat << EOFApache > /etc/apache2/sites-available/wordpress.conf
 <VirtualHost *:80>
     DocumentRoot /srv/www/wordpress
     <Directory /srv/www/wordpress>
@@ -130,36 +131,45 @@ EOF
     </Directory>
 </VirtualHost>
 EOFApache'
-            sudo a2ensite wordpress
-            sudo a2enmod rewrite
-            sudo a2dissite 000-default
-            sudo systemctl reload apache2
-            
-            sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
-            sudo -u www-data sed -i 's/database_name_here/wordpress/' /srv/www/wordpress/wp-config.php
-            sudo -u www-data sed -i 's/username_here/wordpress/' /srv/www/wordpress/wp-config.php
-            sudo -u www-data sed -i "s/password_here/<your-password>/" /srv/www/wordpress/wp-config.php
-            
-            sudo -u www-data curl -s https://api.wordpress.org/secret-key/1.1/salt/ | sudo -u www-data tee -a /srv/www/wordpress/wp-config.php > /dev/null
-            
-            echo "** WordPress installatie op $server voltooid."
+                    sudo a2ensite wordpress
+                    sudo a2enmod rewrite
+                    sudo a2dissite 000-default
+                    sudo systemctl reload apache2
+                    echo "Installatie op $server voltooid."
+                    ((succes_aantal++))
+                else
+                    echo "Installatie op $server mislukt."
+                    ((installatie_mislukt_aantal++))
+                fi
+
+                echo "** MySQL database configureren **"
+                sudo mysql -u root <<MYSQL_SCRIPT
+CREATE DATABASE wordpress;
+CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+                echo "MySQL database configuratie voltooid op $server."
+
 EOF
+        fi
         lijn_scheider
     done
 
     lijn_scheider
     groene_echo "** SAMENVATTING **"
     echo
-    groene_echo "WordPress succesvol geïnstalleerd op $succes_aantal server(s)."
+    groene_echo "Apache succesvol gestart op $succes_aantal server(s)."
+    blauwe_echo "Apache draait al op $al_geactiveerd_aantal server(s):$al_geactiveerd_servers"
+    oranje_echo "Apache was al geïnstalleerd maar niet actief op:$al_geïnstalleerd_niet_actief_servers"
     rode_echo "Totaal aantal installatiefouten: $installatie_mislukt_aantal"
     lijn_scheider
     echo
-    groene_echo "U kunt de WordPress-site bereiken op de volgende URL(s):"
+    blauwe_echo "U kunt de WordPress-site bereiken op de volgende URL(s):"
     for server in "${servers[@]}"; do
-        echo "http://$server/wp-admin"
+        blauwe_echo "http://$server/wp-admin"
     done
-    lijn_scheider
-    echo
+
 elif [ "$keuze" == "n" ]; then
     oranje_echo "Gebruiker heeft het script afgebroken."
 else
