e4a09917d79b949189035dcc34f1758d67b802ad
Sten Tijhuis <102481635+Stensel8@users.noreply.github.com>
2025-01-28 17:24:56 +0100

    Simplified

--- Scripts/scriptmaster-files/Week 2/installminecraftonwindows.ps1
+++ Scripts/scriptmaster-files/Week 2/installminecraftonwindows.ps1
@@ -17,7 +17,7 @@ $modsFolder = Join-Path $env:APPDATA ".minecraft\mods"
 
 # Function to open the default web browser with the OptiFine download link
 function Open-OptiFineDownloadLink {
-    $optiFineDownloadUrl = "https://optifine.net/adloadx?f=OptiFine_1.20.4_HD_U_I7.jar&x=8d29"
+    $optiFineDownloadUrl = "https://optifine.net/downloads"
     Write-Host "Opening OptiFine download link in the default web browser..." -ForegroundColor Cyan
     Start-Process $optiFineDownloadUrl
 }
@@ -31,22 +31,15 @@ function Install-OptiFine {
     Show-Popup "Please download OptiFine from the opened webpage and manually install it into the Minecraft mods folder at: $modsFolder and then doubleclick to execute it. Make sure to select OptiFine via the Minecraft Launcher after installation." "OptiFine Installation" "OK" "Information"
     $choiceMinecraft = Show-Popup "Head over to the Microsoft Store to download Minecraft?" "Minecraft Installation" "YesNo" "Question"
 
-    # Check the user's choice regarding Minecraft installation
-    if ($choiceMinecraft -eq "Yes") {
-        # Open Microsoft Store for Minecraft installation
-        Start-Process ms-windows-store://pdp/?ProductId=9PGW18NPBZV5
-    } else {
-        Write-Host "User chooses not to go to the Microsoft Store. Exiting..." -ForegroundColor Yellow
-    }
 }
 
 # Display the popup message to ask about installing Minecraft
-$choiceMinecraft = Show-Popup "Do you want to install Minecraft for Windows 11? This will open the Microsoft Store, as scripted Minecraft installers are not supported by Microsoft."
+$choiceMinecraft = Show-Popup "Do you want to install Minecraft? This will open the Microsoft Store, as scripted Minecraft installers are not supported by Microsoft."
 
 # Check the user's choice regarding Minecraft installation
 if ($choiceMinecraft -eq "Yes") {
     # Display the popup message to ask about installing OptiFine
-    $choiceOptiFine = Show-Popup "Do you want to install the OptiFine plugin for Minecraft version 1.20.4?"
+    $choiceOptiFine = Show-Popup "Do you want to install the OptiFine plugin for Minecraft?"
 
     if ($choiceOptiFine -eq "Yes") {
         # Open the default web browser to download OptiFine
